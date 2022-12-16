dfee691631aaaa9a37feb592949e8b3927ffb973
Stas <112703038+silenthnowakeup@users.noreply.github.com>
2022-12-17 00:46:25 +0300

    Update main.c

--- lab8Sonar/main.c
+++ lab8Sonar/main.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "library.h"
 
 int main(int argc, char** argv)
 {
+    int size = argc - 1;
     char** str = (char**)calloc(argc - 1, sizeof(char*));
-    int* arr = (int*)calloc(argc - 1, sizeof(int));
-    for (int i = 1; i < argc; i++)
-        str[i - 1] = copyString(argv[i], str[i - 1]);
-    for (int i = 0; i < argc - 1; i++)
-        arr[i] = length(str[i]);
-    merge_sort(arr, 0, argc - 1);
-    printf("Result::\n");
-    str = sorting(str, arr, argc);
-    printStrings(argc, str);
+    int** arr = (int**)calloc(2, sizeof(int*));
+    arr[0] = (int*)calloc(argc - 1, sizeof(int));
+    arr[1] = (int*)calloc(argc - 1, sizeof(int));
+    for(int i = 0; i < size; i++)
+        arr[0][i] = i;
+    for(int i = 1; i < argc; i++)
+        str[i - 1] = argv[i];
+    lengthStr(arr, str, size);
+    merge_sort(arr, 0, size);
+    sorting(str, arr, size);
     return 0;
 }
